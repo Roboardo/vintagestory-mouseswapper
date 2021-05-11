c5743e93906fd2749f23b470c58a26a6c6e0c886
Lyrth <defelixanne@gmail.com>
2021-05-12 00:54:22 +0800

    Some refactoring lol

--- src/MouseSwapper.cs
+++ src/MouseSwapper.cs
@@ -2,88 +2,85 @@
 using Vintagestory.API.Common;
 using Vintagestory.Client.NoObf;
 using HarmonyLib;
-using static HarmonyLib.AccessTools;
 
-namespace mouseswapper
+namespace MouseSwapper
 {
     class MouseSwapper : ModSystem
     {
-        ICoreClientAPI _clientApi;
         public override void StartClientSide(ICoreClientAPI api)
         {
-            _clientApi = api;
-            MyPatcher.DoPatching();
+            Patcher.DoPatching();
             System.Diagnostics.Debug.WriteLine("I like 4 limbed dragons");
         }
     }
 
-    public class MyPatcher
+
+    public class Patcher
     {
         // make sure DoPatching() is called at start either by
         // the mod loader or by your injector
 
         public static void DoPatching()
         {
-            var harmony = new Harmony("com.example.patch");
+            var harmony = new Harmony("vsmods.patch.mouseswapper");
             harmony.PatchAll();
         }
     }
 
+
     [HarmonyPatch(typeof(ClientMain))]
     [HarmonyPatch("OnMouseDown")]
-    class Patch01
+    class MouseDownPatch
     {
         static void Prefix(MouseEvent args, out MouseEvent __state)
         {
             __state = args;
         }
+
         static void Postfix(MouseEvent __state, ref MouseButtonState ___InWorldMouseState)
         {
             ___InWorldMouseState.Left = false;
             ___InWorldMouseState.Right = false;
+            
             if (__state.Button == EnumMouseButton.Left)
-            {
                 ___InWorldMouseState.Right = true;
-            }
+
             if (__state.Button == EnumMouseButton.Right)
-            {
                 ___InWorldMouseState.Left = true;
-            }
         }
     }
 
-    class StateKeeper {
-        public MouseEvent mouseEvent;
-        public bool Left;
-        public bool Right;
-    }
 
     [HarmonyPatch(typeof(ClientMain))]
     [HarmonyPatch("OnMouseUp")]
-    class Patch02
+    class MouseUpPatch
     {
+        struct StateKeeper {
+            public EnumMouseButton Button;
+            public bool Left;
+            public bool Right;
+        }
+
         static void Prefix(MouseEvent args, out StateKeeper __state, ref MouseButtonState ___InWorldMouseState)
         {
-            StateKeeper state = new StateKeeper();
-            state.mouseEvent = args;
-            state.Left = ___InWorldMouseState.Left;
-            state.Right = ___InWorldMouseState.Right;
-            __state = state;
+            __state = new StateKeeper
+            {
+                Button = args.Button, 
+                Left = ___InWorldMouseState.Left, 
+                Right = ___InWorldMouseState.Right,
+            };
         }
+
         static void Postfix(StateKeeper __state, ref MouseButtonState ___InWorldMouseState)
         {
             ___InWorldMouseState.Right = __state.Right;
             ___InWorldMouseState.Left = __state.Left;
 
-            if (__state.mouseEvent.Button == EnumMouseButton.Left)
-            {
+            if (__state.Button == EnumMouseButton.Left)
                 ___InWorldMouseState.Right = false;
-            }
-            
-            if (__state.mouseEvent.Button == EnumMouseButton.Right)
-            {
+
+            if (__state.Button == EnumMouseButton.Right)
                 ___InWorldMouseState.Left = false;
-            }
         }
     }
 }
